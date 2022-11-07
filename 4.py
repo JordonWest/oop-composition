28f55d8d7cc92cfaa95c0c0c02b1662ccb386700
Jordon West <jwest@relmp-jwest1.us.drwholdings.com>
2022-11-07 15:06:00 -0600

    final commit for 4

---
+++ 4.py
@@ -0,0 +1,61 @@
+class Dog:
+    allowed_outside = True
+    is_good_boy = True
+    
+    def __init__(self, age, weight):
+        self.age = age
+        self.weight = weight
+        self.is_old = False
+        self.medication = None
+
+    def age_dog(self):#instance method
+        self.age +=1
+        return self.age
+    
+    def show_meds(self):
+        print(self.medication)
+
+    @classmethod #class method
+    def toggle_allowed(cls):
+        cls.allowed_outside = not cls.allowed_outside
+
+class SmallDog(Dog):
+
+    def __init__(self, age, weight, is_yappy):
+        super().__init__(age, weight)
+        self.is_yappy = is_yappy
+        self.check_weight() #run on instantiation
+    
+    def check_weight(self):
+        if self.weight > 10:
+            print("This is not a small dog!")
+
+class LargeDog(Dog):
+
+    def __init__(self, age, weight, leash_trained):
+        super().__init__(age, weight)
+        self.leash_trained = leash_trained
+    
+    def age_dog(self):
+        self.age += 1
+        if self.age > 5:
+# Lets update 'is_old' to do something
+            #self.is_old = True
+            self.medication=(Meds(1, 'hip medication'))
+
+# ---- Composition (has-a relationship)
+class Meds:
+
+    def __init__(self, number, med_type):
+        self.number = number
+        self.med_type = med_type
+
+    def __str__(self):
+        return f"""
+            Number of meds: {self.number}
+            Type of meds: {self.med_type}
+            """
+dog_5 = LargeDog(5, 50, True)
+dog_5.age_dog()
+print(dog_5.medication)
+
