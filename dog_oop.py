1a2d234d96884d25d6e043e0ca1a2df6bda8d22f
Jordon West <jwest@relmp-jwest1.us.drwholdings.com>
2022-11-07 21:54:54 -0600

    clean-up

--- dog_oop.py
+++ dog_oop.py
@@ -1,58 +1,46 @@
 class Dog:
+    #what do all dogs have
     allowed_outside = True
     is_good_boy = True
-    
+
+    #what do all dogs have (variable to the dog)
     def __init__(self, age, weight):
         self.age = age
         self.weight = weight
         self.is_old = False
-### add this after bottom VVVV
-        self.medication = None
+        self.medication = []
 
-    def age_dog(self):#instance method
+    # instance method
+    def age_dog(self):
         self.age +=1
         return self.age
-
-    @classmethod #class method
+    # Methods that are only invoked on the class - not instances
+    @classmethod
     def toggle_allowed(cls):
         cls.allowed_outside = not cls.allowed_outside
-
 # ------ INHERITANCE  (is-a relationship)
 class SmallDog(Dog):
 
     def __init__(self, age, weight, is_yappy):
         super().__init__(age, weight)
         self.is_yappy = is_yappy
-        self.check_weight() #run on instantiation
-    
+        self.check_weight() #run on instantiation - new thing?
+
     def check_weight(self):
         if self.weight > 10:
             print("This is not a small dog!")
-##          DELETE ALL THIS VVV
-#dog_2 = SmallDog(5, 22, False)
-#dog_3 = SmallDog(5, 8, False)
-##          DELETE ALL THIS ^^^
 
 class LargeDog(Dog):
 
     def __init__(self, age, weight, leash_trained):
         super().__init__(age, weight)
         self.leash_trained = leash_trained
-    
+
     def age_dog(self):
         self.age += 1
         if self.age > 5:
-# Lets update 'is_old' to do something
-            self.medication=(Medication(1, 'hip medication'))
-
-##          DELETE ALL THIS VVV
-#dog_4 = LargeDog(5, 50, True)
-#print(f"LargeDog is {dog_4.age}. Is it old? {dog_4.is_old}")
-#dog_4.age_dog()
-#print(f"LargeDog is {dog_4.age}. Is it old? {dog_4.is_old}")
-##          DELETE ALL THIS ^^^
-
-####        ----- ADD ALL THIS VVVVVVV ------
+            self.is_old = True
+            self.medication=Medication(1, 'hip medication')
 
 # ---- Composition (has-a relationship)
 class Medication:
@@ -61,7 +49,11 @@ class Medication:
         self.number = number
         self.med_type = med_type
 
+    def __str__(self):
+        return f"""
+            Number of meds: {self.number}
+            Type of meds: {self.med_type}
+            """
 dog_5 = LargeDog(5, 50, True)
-print(f"LargeDog is old? {dog_5.is_old} Meds = {dog_5.medication} ")
 dog_5.age_dog()
-print(f"LargeDog is old? {dog_5.is_old} Meds = {dog_5.medication.number} {dog_5.medication.med_type} ")
+print(dog_5.medication)
